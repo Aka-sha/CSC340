9533413bb0af51b01e5eae4684779a218f697ace
William Andrew Cruse <andrew.wcruse@gmail.com>
2021-02-14 21:16:32 -0500

    Improved Food API

--- Food API/src/API/FoodApiTranslator.java
+++ Food API/src/API/FoodApiTranslator.java
@@ -24,6 +24,7 @@ public class FoodApiTranslator implements FoodApiInterface{
      */
     @Override
     public String loadRecipeTitleById(int _id) {
+        // Builds base url string
         String searchString = new String("/recipes/" + _id + "/information?apiKey=" + FoodApiTranslator.apiKey);
         try{
             URL url = new URL(FoodApiTranslator.baseURL + searchString);
@@ -31,21 +32,223 @@ public class FoodApiTranslator implements FoodApiInterface{
             connection.setRequestMethod("GET");
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String inputLine;
-
+            // Build the content from the buffered input
             StringBuffer content = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
                 content.append(inputLine);
             }
-
+            // Close connections
             in.close();
             connection.disconnect();
-
+            // Extract JSON object
             JSONObject obj = new JSONObject(content.toString());
             String title = obj.getString("title");
             return title;
         } catch (Exception ex) {
             return null;
         }
+    }
 
+    /**
+     * This method is used to connect to the Food API via a URL and add the contents to a JSON file.
+     * Then the file is read to a String.
+     * @param _id
+     * @return String summary
+     */
+    @Override
+    public String loadSummaryById(int _id) {
+        // Builds base url string
+        String searchString = new String("/recipes/" + _id + "/information?apiKey=" + FoodApiTranslator.apiKey);
+        try{
+            URL url = new URL(FoodApiTranslator.baseURL + searchString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            // Build the content from the buffered input
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            // Close connections
+            in.close();
+            connection.disconnect();
+            // Extract JSON object
+            JSONObject obj = new JSONObject(content.toString());
+            String summary = obj.getString("summary");
+            return summary;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    /**
+     * This method is used to connect to the Food API via a URL and add the contents to a JSON file.
+     * Then the file is read to a String.
+     * @param _id
+     * @return Boolean vegetarian
+     */
+    @Override
+    public Boolean loadIsVegetarianById(int _id) {
+        // Builds base url string
+        String searchString = new String("/recipes/" + _id + "/information?apiKey=" + FoodApiTranslator.apiKey);
+        try{
+            URL url = new URL(FoodApiTranslator.baseURL + searchString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            // Build the content from the buffered input
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            // Close connections
+            in.close();
+            connection.disconnect();
+            // Extract JSON object
+            JSONObject obj = new JSONObject(content.toString());
+            Boolean vegetarian = obj.getBoolean("vegetarian");
+            return vegetarian;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * This method is used to connect to the Food API via a URL and add the contents to a JSON file.
+     * Then the file is read to a String.
+     * @param _id
+     * @return Boolean vegan
+     */
+    @Override
+    public Boolean loadIsVeganById(int _id) {
+        // Builds base url string
+        String searchString = new String("/recipes/" + _id + "/information?apiKey=" + FoodApiTranslator.apiKey);
+        try{
+            URL url = new URL(FoodApiTranslator.baseURL + searchString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            // Build the content from the buffered input
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            // Close connections
+            in.close();
+            connection.disconnect();
+            // Extract JSON object
+            JSONObject obj = new JSONObject(content.toString());
+            Boolean vegan = obj.getBoolean("vegan");
+            return vegan;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * This method is used to connect to the Food API via a URL and add the contents to a JSON file.
+     * Then the file is read to a String.
+     * @param _id
+     * @return Double pricePerServing
+     */
+    @Override
+    public Double loadPricePerServingById(int _id) {
+        // Builds base url string
+        String searchString = new String("/recipes/" + _id + "/information?apiKey=" + FoodApiTranslator.apiKey);
+        try{
+            URL url = new URL(FoodApiTranslator.baseURL + searchString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            // Build the content from the buffered input
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            // Close connections
+            in.close();
+            connection.disconnect();
+            // Extract JSON object
+            JSONObject obj = new JSONObject(content.toString());
+            Double pricePerServing = obj.getDouble("pricePerServing");
+            return pricePerServing;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * This method is used to connect to the Food API via a URL and add the contents to a JSON file.
+     * Then the file is read to a String.
+     * @param _id
+     * @return Integer readyInMinutes
+     */
+    @Override
+    public Integer loadReadyInMinutesById(int _id) {
+        // Builds base url string
+        String searchString = new String("/recipes/" + _id + "/information?apiKey=" + FoodApiTranslator.apiKey);
+        try{
+            URL url = new URL(FoodApiTranslator.baseURL + searchString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            // Build the content from the buffered input
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            // Close connections
+            in.close();
+            connection.disconnect();
+            // Extract JSON object
+            JSONObject obj = new JSONObject(content.toString());
+            Integer readyInMinutes = obj.getInt("readyInMinutes");
+            return readyInMinutes;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * This method is used to connect to the Food API via a URL and add the contents to a JSON file.
+     * Then the file is read to a String.
+     * @param _id
+     * @return Integer servingsNum
+     */
+    @Override
+    public Integer loadServingsById(int _id) {
+        // Builds base url string
+        String searchString = new String("/recipes/" + _id + "/information?apiKey=" + FoodApiTranslator.apiKey);
+        try{
+            URL url = new URL(FoodApiTranslator.baseURL + searchString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            // Build the content from the buffered input
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            // Close connections
+            in.close();
+            connection.disconnect();
+            // Extract JSON object
+            JSONObject obj = new JSONObject(content.toString());
+            Integer servingsNum = obj.getInt("servings");
+            return servingsNum;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
     }
 }
--- Food API/src/Main.java
+++ Food API/src/Main.java
@@ -2,11 +2,21 @@ import Models.Recipe;
 
 public class Main {
     public static void main(String[] args) {
-        //Recipe goodRecipe = Recipe.loadRecipeTitleById(716429); //returns "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs"
-        //Recipe goodRecipe = Recipe.loadRecipeTitleById(209128); //returns "Dinner Tonight: Grilled Romesco-Style Pork"
-        //Recipe goodRecipe = Recipe.loadRecipeTitleById(498576); //returns "Gnocchi in Parmesan Garlic Sauce"
-        Recipe goodRecipe = Recipe.loadRecipeTitleById(123456); // returns "Apple Delight Canadian 1962"
+        int id = 716429; //returns "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs"
+        Recipe recipeTitle = Recipe.loadRecipeTitleById(id);
+        Recipe recipeSummary = Recipe.loadSummaryById(id);
+        Recipe recipeVegetarian = Recipe.loadIsVegetarianById(id);
+        Recipe recipeVegan = Recipe.loadIsVeganById(id);
+        Recipe recipePricePerServing = Recipe.loadPricePerServingById(id);
+        Recipe recipeMinutes = Recipe.loadReadyInMinutesById(id);
+        Recipe recipeServings = Recipe.loadServingsById(id);
 
-        System.out.printf("Recipe Title: " + goodRecipe.getTitle());
+        System.out.println("Recipe Title: " + recipeTitle.getTitle());
+        System.out.println("Recipe Summary: " + recipeSummary.getSummary());
+        System.out.println("Vegetarian: " + recipeVegetarian.getIsVegetarian());
+        System.out.println("Vegan: " + recipeVegan.getIsVegan());
+        System.out.println("Price Per Serving: " + recipePricePerServing.getPricePerServing());
+        System.out.println("Minutes Needed to Cook: " + recipeMinutes.getReadyInMinutes());
+        System.out.println("Servings: " + recipeServings.getServings());
     }
 }
