7c5754a9fc15f10db56dc2568ccb74eff8de3065
ECHICKS <47527393+ECHICKS@users.noreply.github.com>
2021-02-15 18:15:58 -0500

    FoodAPITranslator & FoodAPIInterface Overhaul
    The translator and interface were overhauled so that any piece of information desired can be called from one generic method instead of several near-identical methods that specialize on one piece of information. Recipe.java was also slightly edited to be compatible with these changes. The output is identical to what it was before.

--- Food API/src/API/FoodApiInterface.java
+++ Food API/src/API/FoodApiInterface.java
@@ -4,22 +4,9 @@ package API;
  * This interface will define methods that are needed for the Food API Translator
  * or any relevant API that is to be implemented.
  * Last Updated 02/14/2021
- * @author Andy Cruse
+ * @author Andy Cruse, modified by Edward Hicks
  */
 public interface FoodApiInterface {
 
-    public String loadRecipeTitleById(int _id);
-
-    public String loadSummaryById(int _id);
-
-    public Boolean loadIsVegetarianById(int _id);
-
-    public Boolean loadIsVeganById(int _id);
-
-    public Double loadPricePerServingById(int _id);
-
-    public Integer loadReadyInMinutesById(int _id);
-
-    public Integer loadServingsById(int _id);
-
+    public Object loadRecipeItem(int _id, String loadItem);
 }
