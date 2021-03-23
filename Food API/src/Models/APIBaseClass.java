a521009aea0ebad02239883694f4ee6902ccf2bd
William Andrew Cruse <andrew.wcruse@gmail.com>
2021-03-23 18:24:51 -0400

    Added Geolocation via IP Address

--- Food API/src/Models/APIBaseClass.java
+++ Food API/src/Models/APIBaseClass.java
@@ -2,15 +2,20 @@ package Models;
 
 import API.FoodApiInterface;
 import API.FoodApiTranslator;
+import API.GeolocationApiInterface;
+import API.GeolocationApiTranslator;
 
 /**
  * This class acts as a base for the API(s) that will be used through the application
  * We will be able to change API for the entire project by editing a line in this class
- * Currently only 1 API is in use. Will add backups and additional API later.
- * Last Update: 02/14/2021
+ * Currently only 2 API is in use, and API for food and an API for Geolocation.
+ * Will add backups and additional API later.
+ * Last Update: 03/23/2021
  * @author Andy Cruse
  */
 public class APIBaseClass {
 
     protected final static FoodApiInterface myFoodAPI = new FoodApiTranslator();
+    protected final static GeolocationApiInterface myLocationAPI = new GeolocationApiTranslator();
+
 }
