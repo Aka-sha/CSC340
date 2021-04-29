48d38fc08625bb7be5f768e88be1de5249050e0c
Kevindu <yikuandu96@gmail.com>
2021-04-30 01:16:23 +0800

    Maybe the last update

--- Nakama Scouter/src/UI/animeListCon.java
+++ Nakama Scouter/src/UI/animeListCon.java
@@ -33,37 +33,30 @@ public class animeListCon {
     int click = 1;
 
     @FXML
-    public void homePage(ActionEvent event) throws Exception {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
-            Scene rooter = new Scene(root);
-            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            window.setScene(rooter);
-            window.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void homePage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        Scene rooter = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(rooter);
+        window.show();
     }
 
     @FXML
-    public void topPage(ActionEvent event) throws Exception {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/fxml/topAnime.fxml"));
-            Parent Parent = loader.load();
+    public void topPage(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/topAnime.fxml"));
+        Parent Parent = loader.load();
 
-            Scene Scene = new Scene(Parent);
+        Scene Scene = new Scene(Parent);
 
+        animeListCon controller = loader.getController();
+        controller.loadImage(1);
 
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
 
-            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(Scene);
+        window.show();
 
-            window.setScene(Scene);
-            window.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 
@@ -82,18 +75,13 @@ public class animeListCon {
     }
 
     @FXML
-    public void loginPage(ActionEvent event) throws Exception {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-            Scene rooter = new Scene(root);
-            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            window.setScene(rooter);
-            window.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void loginPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        Scene rooter = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(rooter);
+        window.show();
     }
-
 
     @FXML
     public void nextList(ActionEvent event) {
@@ -119,21 +107,6 @@ public class animeListCon {
     }
     public void loadImage(int n) {
         AnimeManga anime = preSet();
-        /*t1Anime.setImage(new Image("https://cdn.myanimelist.net/images/anime/1436/106694.jpg?s=d395802efdb5b4a093f094f0090b7a07"));
-        T1name.setText("Itai no wa Iya nano de Bougyoryoku ni Kyokufuri Shitai to Omoimasu. II");
-        T1text.setText("Second season of Itai no wa Iya nano de Bougyoryoku ni Kyokufuri Shitai to Omoimasu.");
-        t2Anime.setImage(new Image("https://cdn.myanimelist.net/images/anime/1407/111469.jpg?s=57f3024519667ef0d0fd15beeea628c1"));
-        T2name.setText("Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka IV");
-        T2text.setText("Fourth season of Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka.");
-        t3Anime.setImage(new Image("https://cdn.myanimelist.net/images/anime/1978/111973.jpg?s=611c7f900ef12f49707e02b5d7435bb4"));
-        T3name.setText("Toku: Touken Ranbu - Hanamaru - Setsugetsuka");
-        T3text.setText("Touken Ranbu: Hanamaru anime movie trilogy.");
-        t4Anime.setImage(new Image("https://cdn.myanimelist.net/images/anime/1340/110088.jpg?s=ee67671d1f7730716efef5ba82681170"));
-        T4name.setText("Ousama Ranking");
-        T4text.setText("The web manga centers around Bojji, a deaf, powerless prince who cannot even wield a children's sword. As the firstborn son, he strives hard and dreams of becoming the world's greatest king. However,...");
-        t5Anime.setImage(new Image("https://cdn.myanimelist.net/images/anime/1236/113727.jpg?s=83919aa2db778f788cb7b7664b802a59"));
-        T5name.setText("Tensei shitara Slime Datta Ken 2nd Season Part 2");
-        T5text.setText("Second half of Tensei shitara Slime Datta Ken 2nd Season.");*/
         t1Anime.setImage(new Image(anime.getImageUrlList().get(n*5-4)));
         T1name.setText(anime.getTitleList().get(n*5-4));
         T1text.setText(anime.getSynopsisList().get(n*5-4));
--- Nakama Scouter/src/UI/signUpCon.java
+++ Nakama Scouter/src/UI/signUpCon.java
@@ -1,5 +1,6 @@
 package UI;
 
+import Models.City;
 import db.UserDatabase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -32,23 +33,19 @@ public class signUpCon {
     private TextField reZip;
 
     @FXML
-    private TextField reCity;
+    private TextField reAddress;
 
     @FXML
-    public void homePage(ActionEvent event) throws Exception {
-        try {
+    public void homePage(ActionEvent event) throws IOException {
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
             Scene rooter = new Scene(root);
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
             window.setScene(rooter);
             window.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @FXML
-    public void welcome(ActionEvent event, String info) throws IOException {
+    public void welcome(ActionEvent event, String info) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, info, new ButtonType("Close", ButtonBar.ButtonData.YES));
         alert.setHeaderText(null);
         alert.setTitle("Welcome");
@@ -56,7 +53,7 @@ public class signUpCon {
     }
 
     @FXML
-    public void alert(ActionEvent event, String info) throws IOException {
+    public void alert(ActionEvent event, String info) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, info, new ButtonType("Close", ButtonBar.ButtonData.YES));
         alert.setHeaderText(null);
         alert.setTitle("Alert");
@@ -65,6 +62,9 @@ public class signUpCon {
 
     @FXML
     void signup(ActionEvent event) throws IOException {
+        UserDatabase userDB = new UserDatabase();
+        userDB.loadUserDatabaseDefault();
+
         String userids = reUserid.getText();
         String passWords = rePass1.getText();
         String Confirm = rePass2.getText();
@@ -74,33 +74,76 @@ public class signUpCon {
         String age = reAge.getText();
         int ageInt = Integer.parseInt(age);
         int min = age.compareTo("18");
-        String city = reCity.getText();
+        String address = reAddress.getText();
+        City city = City.loadCityResultsByAddress(address);
         String empty = "";
+        int error = 0;
+        String info = "";
+
 
 
         if (!passWords.equals(Confirm)) {
-            String info = "Passwords are not matched";
+            info += "Passwords are not matched. \n";
+            error++;
+        }
+        if (userids.length() < 4) {
+            info += "Username must be at least 4 characters in length. \n";
+            error++;
+        }
+        if (passWords.length() < 8) {
+            info += "Password must be at least 8 characters in length. \n";
+            error++;
+        }
+        if (userids.equals(empty) || passWords.equals(empty) || Confirm.equals(empty) || email.equals(empty) || zip.equals(empty) || age.equals(empty) || address.equals((empty)) ) {
+            info += "Please fill all the form. \n";
+            error++;
+        }
+        if (min < 0) {
+            info += "You need larger then 18 years old. \n";
+            error++;
+        }
+        if (userids.contains(" ")){
+            info += "Username must not contain any spaces. \n";
+            error++;
+        }
+        if (checkUsernameForDuplicates(userids)){
+            info += "The username " + userids + " is already being used by another account. \n";
+            error++;
+        }
+        if (passWords.contains(" ")){
+            info += "Password must not contain any spaces. \n";
+            error++;
+        }
+        if (passWords.equals(passWords.toUpperCase()) || passWords.equals(passWords.toLowerCase())){
+            info += "Password must contain both uppercase and lowercase letters. \n";
+            error++;
+        }
+        if (!passWords.contains("0") && !passWords.contains("1") &&!passWords.contains("2") &&!passWords.contains("3") &&!passWords.contains("4") &&!passWords.contains("5") &&!passWords.contains("6") &&!passWords.contains("7") &&!passWords.contains("8") &&!passWords.contains("9")){
+            info += "Password must contain at least one number. \n";
+            error++;
+        }
+        if (userids.contains(";")){
+            info += "Username must not contain a tab. \n";
+            error++;
+        }
+        if (passWords.contains(";")){
+            info += "Password must not contain a tab. \n";
+            error++;
+        }
+        if (email.contains(";")){
+            info += "Email must not contain a tab. \n";
+            error++;
+        }
+        if (address.contains(";")){
+            info += "Address must not contain a tab. \n";
+            error++;
+        }
+        if(error != 0){
             alert(event, info);
-        } else if (userids.length() < 4) {
-            String info = "Username must be at least 4 characters in length.";
-            alert(event, info);
-        } else if (passWords.length() < 8) {
-            String info = "Password must be at least 8 characters in length.";
-            alert(event, info);
-        } else if (userids.equals(empty) || passWords.equals(empty) || Confirm.equals(empty) || email.equals(empty) || zip.equals(empty) || age.equals(empty)) {
-            String info = "Please fill all the form";
-            alert(event, info);
-        } else if (min < 0) {
-            System.out.println(min);
-            String info = "You need larger then 18 years old";
-            alert(event, info);
-        } else {
-            String info = userids + " Welcome To Anime Scout";
-
-
-            UserDatabase userDB = new UserDatabase();
-            userDB.loadUserDatabaseDefault();
-            userDB.addNewApplicationUser(userids, email, passWords, ageInt, city , zipInt);
+        }
+        if(error == 0) {
+            info = userids + " Welcome To Anime Scouter.";
+            userDB.addNewApplicationUser(userids, email, passWords, ageInt, city.getCityTitle(), zipInt);
             System.out.println(userDB.printDatabase());
             userDB.saveUserDatabaseDefault();
 
@@ -111,10 +154,21 @@ public class signUpCon {
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
             window.setScene(rooter);
             window.show();
-            System.out.println(userids + " from " + city  + " joined us");
+            System.out.println(userids + " from " + city.getCityTitle() + " joined us");
         }
     }
 
+    public boolean checkUsernameForDuplicates(String _userName) {
+        UserDatabase userDB = new UserDatabase();
+        userDB.loadUserDatabaseDefault();
+        //First, check if database is empty. If so, return true.
+        if (userDB.getSize() == 0) return false;
+        if (userDB.getIndexByUsername(_userName) != -1) {
+            System.out.println("The username " + _userName + " is already being used by another account.");
+            return true;
+        }
+        return false;
+    }
 
     @FXML
     public void clear(ActionEvent event) {
@@ -124,5 +178,6 @@ public class signUpCon {
         rePass2.clear();
         reAge.clear();
         reZip.clear();
+        reAddress.clear();
     }
 }
