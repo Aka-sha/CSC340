9f668607c62cd5cf58d8a65f127c7394a042a6b4
Kevindu <yikuandu96@gmail.com>
2021-05-01 06:46:19 +0800

    Delete apiKeyConfig.java

--- Nakama Scouter/src/API/apiKeyConfig.java
+++
@@ -1,9 +0,0 @@
-package API;
-
-/**
- * This class holds our API keys. TOP SECRET! Make sure config.java is not in the Git repo!!!
- */
-public class apiKeyConfig {
-    protected String geoAPIKey = "AIzaSyA8z0J4NUg9XsR3l5z3VlIeOEr9ido8fqo";
-    protected String restAPIKey = "6144484d3b41cf035960ad4820487068";
-}
